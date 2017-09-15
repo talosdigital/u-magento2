f1c6ee9ae29632b7a5896bd4d0eaf3f18df7fe43
Carlos Arango <carlos.arango@talosdigital.com>
2017-09-15 10:23:43 -0500

    Add exclude minify js configuration

--- app/design/frontend/Talos/default/requirejs-config.js
+++ app/design/frontend/Talos/default/requirejs-config.js
@@ -2,7 +2,8 @@ var config = {
     map: {
         '*': {
             myScript: 'js/my-script',
-            imagerotator: 'Magento_Theme/js/imagerotator'
+            imagerotator: 'Magento_Theme/js/imagerotator',
+            jwplayer: 'Talos_Minify/js/jw/jwplayer'
         }
     }
 };
--- app/design/frontend/Talos/default/web/js/my-script.js
+++ app/design/frontend/Talos/default/web/js/my-script.js
@@ -2,8 +2,9 @@ define([
     'jquery',
     'underscore',
     'Magento_Checkout/js/model/step-navigator',
+    'jwplayer',
     'imagerotator'
-],function($, _, stepNavigator, imagerotator) {
+],function($, _, stepNavigator, jwplayer, imagerotator) {
     'use strict';
 
     if  (typeof $.ajax !== 'undefined') {
@@ -18,9 +19,14 @@ define([
         console.log('StepNavigator loaded');
     }
 
+    if (typeof jwplayer.call !== 'undefined') {
+        console.log('jwplayer loaded'); // in PRODUCTION(with bundle) works because view.xml exclude.');
+    }
+
     if(typeof imagerotator === 'undefined') {
         console.log('imagerotator is never going to be assigned');
     }
+
     if (typeof _imageRotator.rotate !== 'undefined') {
         console.log('_imageRotator loaded'); // in PRODUCTION(with bundle) works because view.xml exclude.');
     }
